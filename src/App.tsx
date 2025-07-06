0d926cc43a16af9d530a72b5e949428222998fc8
Davina Leong <davina.leong.sy@gmail.com>
2025-07-06 15:21:32 +0800

    Redoing the system

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css"
-import { useState } from "react"
-import { Selector } from "./states/Selector/Selector"
-import type { StateKey } from "./states/Selector/Selector"
 
 function App() {
-  const [selectedState, setSelectedState] = useState<StateKey>("A")
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div>
-        <Selector onSelect={setSelectedState} />
+        <h1>Hello World</h1>
       </div>
     </div>
   )
