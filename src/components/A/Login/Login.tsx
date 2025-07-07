4b50a4977a4845125c34cd1f67f5e9c9761e198e
Davina Leong <davina.leong.sy@gmail.com>
2025-07-07 08:55:38 +0800

    Added a striped background to Login A

--- src/components/A/Login/Login.tsx
+++ src/components/A/Login/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import "./Login.css"
 import logo from "./../../../assets/images/logo-original.png"
 
 interface LoginPageProps {
@@ -21,37 +22,40 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginSuccess }) => {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-pastel-pink-500">
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-xs sm:max-w-sm text-center px-4"
-      >
-        {/* Logo */}
-        <div className="flex justify-center mb-4">
-          <img src={logo} alt="App Logo" className="h-14" />
-        </div>
-
-        {/* App Name */}
-        <h1 className="text-2xl font-serif text-carbon-blue-500 mb-6">
-          App Name
-        </h1>
-
-        {/* Email Input */}
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-sm bg-gray-100"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        {/* Login Button */}
-        <button
-          type="submit"
-          className="w-full bg-black text-white font-semibold py-2"
+    <div className="bg-striped flex min-h-screen justify-center bg-carbon-blue-500">
+      <div className="w-full max-w-screen-lg flex items-center justify-center bg-pastel-pink-500">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-xs sm:max-w-sm text-center px-4"
         >
-          Log In
-        </button>
-      </form>
+          {/* Logo */}
+          <div className="flex justify-center mb-4">
+            <img src={logo} alt="App Logo" className="h-14" />
+          </div>
+
+          {/* App Name */}
+          <h1 className="text-2xl font-serif text-carbon-blue-500 mb-6">
+            App Name
+          </h1>
+
+          {/* Email Input */}
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-sm bg-gray-100"
+          />
+
+          {/* Login Button */}
+          <button
+            type="submit"
+            className="w-full bg-black text-white font-semibold py-2"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
